fc07a86379cfbfa836b4ecf2f0b9530afa7fe945
BenjiChejtman <46895375@est.ort.edu.ar>
2022-05-19 14:36:35 -0300

    Initial commit

---
+++ Assets/ActivateDeactivate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateDeactivate : MonoBehaviour
+{
+    public GameObject myObject; //si hay myObject lo cambio por la luz por ejemplo (en unity, en el inspector), voy a poder desactivar la luz.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     //   myObject.SetActive(false); //desactivamos un objeto por codigo. Lo mismo que apretar la check box de arriba del gameobject en el inspector.
+    }
+
+    // Update is called once per frame
+    void Update() //se ejecuta muchas veces por segundo, para fijarse si el usuario apreto tal tecla.
+    {
+        if (Input.GetKeyDown(KeyCode.Space))//con este if, si se presiona el espacio, desaparece el objeto es decir, se DESACTIVA EL OBJETO. //GetKey, se fija si la tecla se apreto, solto o mantuvo presionada y GetKeyDown, detecta cuando el usuario presiona o BAJA una tecla. GetKey da true todo el tiempo y GetKeyDown da true cuando se hunde o presiona una tecla.
+        {
+            /* if (myObject.activeInHierarchy) //si el objeto esta activado, lo desactivo con lo que se encuntra adentro del if
+             {
+                 myObject.SetActive(false);
+             }
+             else                           //con este else, si no esta activado lo activo
+             {
+                 myObject.SetActive(true);
+             }
+             */
+
+            //setActive= activa o desactiva, al base a una condicion.
+            myObject.SetActive(!myObject.activeInHierarchy); //esto es lo mismo que el if de arriba solo que se hace en una sola linea y mas facil. Activa al cubo al presionar el espacio si esta desactivado, y lo desactiva si esta activado.
+        }
+    }
+
+}
---
+++ Assets/TransformsManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformsManager : MonoBehaviour
+{
+    public GameObject myCube;
+
+    //objetos de los que quiero tomar propiedades
+
+    public GameObject rotado;
+    public GameObject escalado;
+    public GameObject rotadoYescalado;
+
+    public Vector3 originalRotation;
+    public Vector3 originalScale;
+
+    public bool isRotated;
+    public bool isScaled;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isRotated = false;
+        isScaled = false;
+        originalRotation = myCube.transform.eulerAngles;
+        originalScale = myCube.transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R)) //r de rotar
+        {
+            if (isRotated)
+            {
+                myCube.transform.eulerAngles = originalRotation; //le doy la rotacion original
+                isRotated = false; //aclaro que no esta rotado en el bool, declarandolo como false.
+            }
+            else
+            {
+                myCube.transform.eulerAngles = rotado.transform.eulerAngles; //le doy la rotacion de "rotado" a mi cubo, es in traspaso de propiedades, en este caso es un traspaso de rotacion.
+                isRotated = true; //aclaro que esta rotado en el bool, declarandolo como true.
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (isScaled)
+            {
+                myCube.transform.localScale = originalScale;
+                isScaled = false;
+            }
+            else
+            {
+                myCube.transform.localScale = escalado.transform.localScale;
+                isScaled = true;
+            }
+        }
+    }
+}
